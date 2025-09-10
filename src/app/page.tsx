705f892911c103f26dc9a235dabf757e22bdad2e
legendarypenguinmaster <redsky030428@gmail.com>
2025-09-11 04:53:34 +0700

    chore: comment out Footer component in Home page for future implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <StatsSection />
       <TestimonialsSection />
       <CallToActionSection />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
